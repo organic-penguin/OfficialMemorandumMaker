588557c409f350c3054fd2438fd5553db34af07e
Austin Beaty <austin@beaty.tech>
2020-03-15 21:57:09 +0000

    Updates

---
+++ precache-manifest.33b41a82a1a448d55a886c7f6c529852.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07004c4153df2f5b2b55d86fb47d1664",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "8ef3204df0d49a6e5f51",
+    "url": "/my-app/static/css/main.0ddf4b69.chunk.css"
+  },
+  {
+    "revision": "dc8b0252c3226b78f2ba",
+    "url": "/my-app/static/js/2.66f25cce.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/my-app/static/js/2.66f25cce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ef3204df0d49a6e5f51",
+    "url": "/my-app/static/js/main.ed7858ee.chunk.js"
+  },
+  {
+    "revision": "c92d1c9ce7a243c2a4d2",
+    "url": "/my-app/static/js/runtime-main.39d7b4bc.js"
+  }
+]);
