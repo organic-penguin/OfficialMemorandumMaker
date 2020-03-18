b3379e51db3fcf4dc539211557a73d8a3aadcfa6
Austin Beaty <austin@beaty.tech>
2020-03-18 14:32:55 +0000

    Updates

---
+++ precache-manifest.26b66e88cbe4e88935f11223298dacf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69fe5efa15a61d6b40d2d1f7819f438f",
+    "url": "/OfficialMemorandumMaker/home/index.html"
+  },
+  {
+    "revision": "d2e606f9e47a52f0f1d9",
+    "url": "/OfficialMemorandumMaker/home/static/css/main.0ddf4b69.chunk.css"
+  },
+  {
+    "revision": "e5c169e53b1b9b506549",
+    "url": "/OfficialMemorandumMaker/home/static/js/2.798cc066.chunk.js"
+  },
+  {
+    "revision": "895a81af6463f88bd4376e7e7ec73fd2",
+    "url": "/OfficialMemorandumMaker/home/static/js/2.798cc066.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2e606f9e47a52f0f1d9",
+    "url": "/OfficialMemorandumMaker/home/static/js/main.4eefabdc.chunk.js"
+  },
+  {
+    "revision": "413de13530d161d0b261",
+    "url": "/OfficialMemorandumMaker/home/static/js/runtime-main.1c8b2a48.js"
+  }
+]);
